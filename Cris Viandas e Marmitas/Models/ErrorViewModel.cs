bb10816e52edb1f1768f31dec22a8c9f27ec18af
Vitor Hoff <vitorhoff05@gmail.com>
2020-11-30 19:35:11 -0300

    Commit de teste

---
+++ Cris Viandas e Marmitas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cris_Viandas_e_Marmitas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
